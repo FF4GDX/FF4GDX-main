5b37da2b5ca6aa25e97e1fe16c5a62fb96b9c429
Daniel Connolly <dconnolly.au@gmail.com>
2023-01-18 13:45:21 +1100

    Remove VisUI, initial javacpp-presets-ffmpeg setup

--- player/src/main/java/com/cosmicdan/gdxff/Init.java
+++ player/src/main/java/com/cosmicdan/gdxff/Init.java
@@ -1,102 +1,76 @@
 package com.cosmicdan.gdxff;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.kotcrab.vis.ui.VisUI;
-import com.kotcrab.vis.ui.VisUI.SkinScale;
-import com.kotcrab.vis.ui.widget.Menu;
-import com.kotcrab.vis.ui.widget.MenuBar;
-import com.kotcrab.vis.ui.widget.MenuItem;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Init extends ApplicationAdapter {
-	private MenuBar menuBar;
-	private Stage stage;
+public class Init implements ApplicationListener {
+	private Stage stage = null;
+	private Image videoImage = null;
 
+	/**
+	 * Called when the {@link Application} is first created.
+	 */
 	@Override
-	public void create () {
-		VisUI.setSkipGdxVersionCheck(true);
-		VisUI.load(SkinScale.X1);
-
+	public void create() {
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);
 
-		Table root = new Table();
+		final Table root = new Table();
 		root.setFillParent(true);
 		stage.addActor(root);
-
-		menuBar = new MenuBar();
-		root.add(menuBar.getTable()).growX().row();
-		root.add().grow();
-
-		createMenus();
-
-		stage.addActor(new TestWindow());
+		videoImage = new Image();
+		root.add(videoImage).fill().align(Align.center);
 	}
 
-	private void createMenus () {
-		Menu startTestMenu = new Menu("start test");
-		Menu fileMenu = new Menu("file");
-		Menu editMenu = new Menu("edit");
-
-		startTestMenu.addItem(new MenuItem("listview", new ChangeListener() {
-			@Override
-			public void changed (ChangeEvent event, Actor actor) {
-				stage.addActor(new TestListView());
-			}
-		}));
-
-		startTestMenu.addItem(new MenuItem("tabbed pane", new ChangeListener() {
-			@Override
-			public void changed (ChangeEvent event, Actor actor) {
-				stage.addActor(new TestTabbedPane());
-			}
-		}));
-
-		startTestMenu.addItem(new MenuItem("collapsible", new ChangeListener() {
-			@Override
-			public void changed (ChangeEvent event, Actor actor) {
-				stage.addActor(new TestCollapsible());
-			}
-		}));
-
-		//creating dummy menu items for showcase
-		fileMenu.addItem(new MenuItem("menuitem #1"));
-		fileMenu.addItem(new MenuItem("menuitem #2").setShortcut("f1"));
-		fileMenu.addItem(new MenuItem("menuitem #3").setShortcut("f2"));
-
-		editMenu.addItem(new MenuItem("menuitem #4"));
-		editMenu.addItem(new MenuItem("menuitem #5"));
-		editMenu.addSeparator();
-		editMenu.addItem(new MenuItem("menuitem #6"));
-		editMenu.addItem(new MenuItem("menuitem #7"));
-
-		menuBar.addMenu(startTestMenu);
-		menuBar.addMenu(fileMenu);
-		menuBar.addMenu(editMenu);
-	}
-
+	/**
+	 * Called when the {@link Application} is resized. This can happen at any point during a non-paused state but will never
+	 * happen before a call to {@link #create()}.
+	 *
+	 * @param width  the new width in pixels
+	 * @param height the new height in pixels
+	 */
 	@Override
-	public void resize (int width, int height) {
-		stage.getViewport().update(width, height, true);
+	public void resize(int width, int height) {
+
 	}
 
+	/**
+	 * Called when the {@link Application} should render itself.
+	 */
 	@Override
-	public void render () {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-		stage.draw();
+	public void render() {
+
 	}
 
+	/**
+	 * Called when the {@link Application} is paused, usually when it's not active or visible on-screen. An Application is also
+	 * paused before it is destroyed.
+	 */
 	@Override
-	public void dispose () {
-		VisUI.dispose();
-		stage.dispose();
+	public void pause() {
+
+	}
+
+	/**
+	 * Called when the {@link Application} is resumed from a paused state, usually when it regains focus.
+	 */
+	@Override
+	public void resume() {
+
+	}
+
+	/**
+	 * Called when the {@link Application} is destroyed. Preceded by a call to {@link #pause()}.
+	 */
+	@Override
+	public void dispose() {
+
 	}
 }
